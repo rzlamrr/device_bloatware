be0ad7169521be46c5d43c4501157b78c759b27c
Mocarafee <harif.rp05@gmail.com>
2020-11-26 12:48:09 +0700

    Bloatware: Update Readme and config also add Google Keyboard needed file

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # "Device Bloatware" aka my addition app 
 
-to add, just clone to device/bloatware, also add this line to your device tree
+## Introduction
+As in the past i'm doing some rom build, sometimes i got the rom i build is missing some apps which i need.
+Or, the rom has prebuilt apps which too basic (You know, like some AOSP apps). So, the first thing
+I do is make makefile and copying the apps i need to device tree. But as more i add the app, the bigger
+my device tree repo size, and i got difficult time to sync my device tree like when i got limited data, or
+my ISP has slow speed. So after that, I got an idea to separate it from my device tree, and just like that,
+this repo is created.
 
-$(call inherit-product-if-exists, device/bloatware/config.mk)
+## How to use
+
+#### 1. clone this repo to device/bloatware (Why this path? the path name seems funny for me)
+#### 2. add this line to your device tree, either in device.mk or <romname>_<devicename>.mk
+
+		$(call inherit-product-if-exists, device/bloatware/config.mk)
+		
+#### 3. uncomment/comment or maybe delete some apps which you didn't need, for example
+
+#####	 To disable, add # to the product_packages and also the packages
+	# Via Browser
+    #PRODUCT_PACKAGES += \
+    #   Via
+	
+#####	 To enable, remove # near the product_packages and also the packages (Don't remove the # on the description, as it would make build failed)
+	# Via Browser
+    PRODUCT_PACKAGES += \
+       Via
+	   
+Okay, just like that and happy build.
--- config.mk
+++ config.mk
@@ -1,11 +1,43 @@
+# CameraGO, disabled by default
+#PRODUCT_PACKAGES += \
+#    CameraGo \
+
+# Browser selection, please disable or delete one. By default is shipping Via Browser.
+# FirefoxLite
+#PRODUCT_PACKAGES += \
+#    FirefoxLite
+
+# Via Browser
 PRODUCT_PACKAGES += \
-    Abstruct \
-    CameraGo \
+    Via
+
+# Keyboard selection, by enabling GKeyboard you'll removing LatinIME aka AOSP default keyboard and replace it with Google Keyboard instead.	
+#PRODUCT_PACKAGES += \
+#    GKeyboard 
+
+# Gboard configuration, dont forget to uncomment this if you prefer to using GKeyboard
+#PRODUCT_PRODUCT_PROPERTIES += \
+#    ro.com.google.ime.bs_theme=true \
+#    ro.com.google.ime.theme_id=5 \
+#    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
+
+# Also GKeyboard Addition, don't forget to enable this if you using GKeyboard
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/usr,$(TARGET_COPY_OUT_PRODUCT)/usr)
+
+# Google Contacts, I like it because the icon, also the UI compared to AOSP ones. Disable by default
+#PRODUCT_PACKAGES += \
+#    GContacts
+
+# My Preffered Bloated Apps
+PRODUCT_PACKAGES += \
     GalleryGo \
-    GContacts \
-    GKeyboard \
-    FirefoxLite \
-    Via \
     KimchiRecorder \
-    Musicolet \
-    RemovePackages
+    Musicolet 
+	
+# Wallpaper apps like AOSPA ones, enable it if you like AOSPA/Oneplus styled wallpaper
+#PRODUCT_PACKAGES += \
+#    Abstruct 
+
+# Remove unused packages, by default i'm adding some LineageOS apps like audiofx, jelly, and some gapps prebuilt apps like Google TTS
+#PRODUCT_PACKAGES += \
+#    RemovePackages
