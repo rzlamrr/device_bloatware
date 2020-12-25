422b29a4782c846c48ea1d517b586db1f819abc6
rzlamrr <rizal82rebel@gmail.com>
2020-12-26 06:38:38 +0700

    app: add MixPlorer, RetroMusic, MiCalculator, QKSMS and Google Dialer update Via remove unnecessary app for me
    Signed-off-by: rzlamrr <rizal82rebel@gmail.com>

--- config.mk
+++ config.mk
@@ -12,31 +12,40 @@ PRODUCT_PACKAGES += \
     Via
 
 # Keyboard selection, by enabling GKeyboard you'll removing LatinIME aka AOSP default keyboard and replace it with Google Keyboard instead.	
-#PRODUCT_PACKAGES += \
-#    GKeyboard 
+PRODUCT_PACKAGES += \
+    GKeyboard
 
 # Gboard configuration, dont forget to uncomment this if you prefer to using GKeyboard
-#PRODUCT_PRODUCT_PROPERTIES += \
-#    ro.com.google.ime.bs_theme=true \
-#    ro.com.google.ime.theme_id=5 \
-#    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
 
 # Also GKeyboard Addition, don't forget to enable this if you using GKeyboard
-#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/usr,$(TARGET_COPY_OUT_PRODUCT)/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/usr,$(TARGET_COPY_OUT_PRODUCT)/usr)
 
 # Google Contacts, I like it because the icon, also the UI compared to AOSP ones. Disable by default
-#PRODUCT_PACKAGES += \
-#    GContacts
+PRODUCT_PACKAGES += \
+    GContacts
+
+# Google Dialer
+PRODUCT_PACKAGES += \
+    GPhone
 
 # My Preffered Bloated Apps
 PRODUCT_PACKAGES += \
     GalleryGo \
-    KimchiRecorder \
-    Musicolet 
-	
+    MixPlorer \
+    QKSMS \
+    MiCalculator
+
+# MixPlorer permissions
+PRODUCT_COPY_FILES += \
+    etc/permissions/privapp-permissions-mixplorer.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mixplorer.xml
+
 # Wallpaper apps like AOSPA ones, enable it if you like AOSPA/Oneplus styled wallpaper
 #PRODUCT_PACKAGES += \
-#    Abstruct 
+#    Abstruct
 
 # Remove unused packages, by default i'm adding some LineageOS apps like audiofx, jelly, and some gapps prebuilt apps like Google TTS
 #PRODUCT_PACKAGES += \
